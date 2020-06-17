87c86af487cc0333046466bf1497ae0c16e015bf
David Occam <davidaoccam@gmail.com>
2020-06-16 23:08:17 -0400

    logo fix not showing part 3

--- dist/assets/js/header.js
+++ dist/assets/js/header.js
@@ -1,7 +1,7 @@
 // header
 if ( document.getElementById("header") ) {
     document.getElementById("header").innerHTML = "\
-    <a href=\"index.html\" class=\"logo\"><img class=\"image fit\" src=\"/images/LogoConstruYa.png\" alt=\"Constru-Ya\"></a>\
+    <a href=\"index.html\" class=\"logo\"><img class=\"image fit\" src=\"/constru-ya/images/LogoConstruYa.png\" alt=\"Constru-Ya\"></a>\
     <ul class=\"icons\">\
         <li><a href=\"#\" class=\"icon brands fa-twitter\"><span class=\"label\">Twitter</span></a></li>\
         <li><a href=\"#\" class=\"icon brands fa-facebook-f\"><span class=\"label\">Facebook</span></a></li>\
